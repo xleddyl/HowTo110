f7d09ca0c6c557af8743bf5686305c95c94b176a
Edoardo Alberti <edoalberti99@gmail.com>
2022-08-11 14:29:54 +0200

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# How To 110
+
+Simple tool for calculating the weighted average of your grades and the one of the exams not yet taken in order to reach 110 at graduation (assuming max points for your thesis).
+
+## Instructions
+
+1. Edit [grades.txt](./grades.txt) and add your your marks, CFUs and the maximum points obtainable with the thesis by following the instructions contained inside the file;
+2. Run [main.py](./main.py);
+3. Results are displayed in the console;
---
+++ main.py
@@ -0,0 +1,48 @@
+def loadFile(fileName):
+   grades = []
+   thesisPoints = 0
+   with open(fileName) as file:
+      for line in file:
+         if line[0] == '#':
+            continue
+         try:
+            if line[0] == 'T':
+               thesisPoints = int(line.strip().split(';')[1])
+               continue
+            grade = int(line.strip().split(';')[0])
+            if grade < 18 or grade > 30: grade = 0
+            CFU = int(line.strip().split(';')[1])
+            grades.append((grade, CFU))
+         except:
+            print('Terminated with error (invalid <GRADE>;<CFU> or T;<MAXPOINTS>) :(')
+            exit(0)
+   return grades, thesisPoints
+
+def computeAvg(grades):
+   numerator = 0
+   CFUs = 0
+   avg = 0
+   for exam in grades:
+      numerator += exam[0] * exam[1]
+      CFUs += exam[1]
+   if CFUs != 0:
+      avg = numerator / CFUs
+   return avg, CFUs
+
+def splitGrades(grades):
+   finalGrades = []
+   pendingGrades = []
+   [(pendingGrades.append(grade) if grade[0] == 0 else finalGrades.append(grade)) for grade in grades]
+   return finalGrades, pendingGrades
+
+if __name__ == '__main__':
+   grades, thesisPoints = loadFile('./grades.txt')
+   finalGrades, pendingGrades = splitGrades(grades)
+   GOAL = ((110 - thesisPoints) * 30) / 110
+   CURRENT, CFU_CURRENT = computeAvg(finalGrades)
+   _, pendingCFUs = computeAvg(pendingGrades)
+   print(f"Exams avg: {round(CURRENT, 2)}")
+   print(f"Goal (assuming {thesisPoints} points for Thesis): {round(GOAL, 2)}")
+   print(f"CFU obtained: {round(CFU_CURRENT, 2)} / Pending CFUs: {pendingCFUs}")
+   PENDING_GOAL = ((((CFU_CURRENT + pendingCFUs) * GOAL) - (CURRENT * CFU_CURRENT)) / pendingCFUs) if pendingCFUs != 0 else 0
+   print(f"Pending exams goal: {round(PENDING_GOAL, 2) if PENDING_GOAL <= 30 and pendingCFUs > 0 else 'impossible, greater than 30 or no more pending CFUs :('}")
